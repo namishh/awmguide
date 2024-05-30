fa43c0c617c2d7cb55ffa3e9c802d3aa51f2a905
nam <namish@tuta.io>
2024-05-30 23:28:03 +0530

    ok

--- theme/init.lua
+++ theme/init.lua
@@ -1,45 +1,68 @@
 -- Theme handling library
-local beautiful           = require('beautiful')
+local beautiful                                 = require('beautiful')
 -- Standard awesome library
-local gears               = require('gears')
+local gears                                     = require('gears')
 
-local theme               = {}
+local theme                                     = {}
 
-theme.sans                = "Lexend"
-theme.font                = "Lexend"
-theme.mono                = "Iosevka Nerd Font"
+theme.sans                                      = "Lexend"
+theme.font                                      = "Lexend"
+theme.mono                                      = "Iosevka Nerd Font"
 
-theme.bg                  = "#141414"
-theme.altbg               = "#222222"
-theme.fg                  = "#f8f8f8"
-theme.comment             = "#585858"
-theme.red                 = "#ab4642"
-theme.blue                = "#7cafc2"
-theme.green               = "#a1b56c"
-theme.yellow              = "#f7ca88"
-theme.magenta             = "#ba8baf"
-theme.cyan                = "#86c1b9"
+theme.bg                                        = "#141414"
+theme.altbg                                     = "#222222"
+theme.fg                                        = "#f8f8f8"
+theme.comment                                   = "#585858"
+theme.red                                       = "#ab4642"
+theme.blue                                      = "#7cafc2"
+theme.green                                     = "#a1b56c"
+theme.yellow                                    = "#f7ca88"
+theme.magenta                                   = "#ba8baf"
+theme.cyan                                      = "#86c1b9"
 
-theme.taglist_bg_focus    = theme.blue
-theme.taglist_bg_empty    = theme.comment
-theme.taglist_bg_occupied = theme.comment
-theme.taglist_bg_urgent   = theme.red
+theme.taglist_bg_focus                          = theme.blue
+theme.taglist_bg_empty                          = theme.comment
+theme.taglist_bg_occupied                       = theme.comment
+theme.taglist_bg_urgent                         = theme.red
 
-theme.useless_gap         = 5
+theme.useless_gap                               = 5
 
-theme.wallpaper           = gears.filesystem.get_configuration_dir() .. 'theme/wall.jpg'
-theme.pfp                 = gears.filesystem.get_configuration_dir() .. 'theme/pfp.jpeg'
+theme.wallpaper                                 = gears.filesystem.get_configuration_dir() .. 'theme/wall.jpg'
+theme.pfp                                       = gears.filesystem.get_configuration_dir() .. 'theme/pfp.jpeg'
 
-theme.iconpath            = gears.filesystem.get_configuration_dir() .. "theme/icons/"
+theme.iconpath                                  = gears.filesystem.get_configuration_dir() .. "theme/icons/"
 
-theme.layout_floating     = theme.iconpath .. "floating.png"
-theme.layout_tile         = theme.iconpath .. "tile.png"
+theme.layout_floating                           = theme.iconpath .. "floating.png"
+theme.layout_tile                               = theme.iconpath .. "tile.png"
 
-theme.wifi_connected      = gears.color.recolor_image(theme.iconpath .. "wifi-connected.svg", theme.fg)
-theme.wifi_disconnected   = gears.color.recolor_image(theme.iconpath .. "wifi-disconnected.svg", theme.comment)
+theme.wifi_connected                            = gears.color.recolor_image(theme.iconpath .. "wifi-connected.svg",
+  theme.fg)
+theme.wifi_disconnected                         = gears.color.recolor_image(theme.iconpath .. "wifi-disconnected.svg",
+  theme.comment)
 
-theme.bluetooth_on        = gears.color.recolor_image(theme.iconpath .. "bluetooth-on.svg", theme.fg)
-theme.bluetooth_off       = gears.color.recolor_image(theme.iconpath .. "bluetooth-off.svg", theme.comment)
+theme.bluetooth_on                              = gears.color.recolor_image(theme.iconpath .. "bluetooth-on.svg",
+  theme.fg)
+theme.bluetooth_off                             = gears.color.recolor_image(theme.iconpath .. "bluetooth-off.svg",
+  theme.comment)
 
+theme.titlebar_close_button_normal              = gears.color.recolor_image(theme.iconpath .. "close.svg",
+  theme.comment)
+theme.titlebar_close_button_focus               = gears.color.recolor_image(theme.iconpath .. "close.svg",
+  theme.red)
+
+theme.titlebar_maximized_button_normal_inactive = gears.color.recolor_image(
+  theme.iconpath .. "maximize.svg",
+  theme.comment)
+theme.titlebar_maximized_button_focus_inactive  = gears.color.recolor_image(
+  theme.iconpath .. "maximize.svg",
+  theme.blue)
+theme.titlebar_maximized_button_normal_active   = gears.color.recolor_image(
+  theme.iconpath .. "maximize.svg",
+  theme.comment)
+theme.titlebar_maximized_button_focus_active    = gears.color.recolor_image(
+  theme.iconpath .. "maximize.svg",
+  theme.blue)
 -- Themes define colors, icons, font and wallpapers.
 beautiful.init(theme)
+beautiful.init(theme)
+
--- ui/titlebar/normal.lua
+++ ui/titlebar/normal.lua
@@ -3,42 +3,40 @@ local wibox = require('wibox')
 
 --- The titlebar to be used on normal clients.
 return function(c)
-   -- Buttons for the titlebar.
-   local buttons = {
-      awful.button(nil, 1, function()
-         c:activate({ context = 'titlebar', action = 'mouse_move' })
-      end),
-      awful.button(nil, 3, function()
-         c:activate({ context = 'titlebar', action = 'mouse_resize' })
-      end)
-   }
+  -- Buttons for the titlebar.
+  local buttons = {
+    awful.button(nil, 1, function()
+      c:activate({ context = 'titlebar', action = 'mouse_move' })
+    end),
+    awful.button(nil, 3, function()
+      c:activate({ context = 'titlebar', action = 'mouse_resize' })
+    end)
+  }
 
-   -- Draws the client titlebar at the default position (top) and size.
-   awful.titlebar(c).widget = wibox.widget({
-      layout = wibox.layout.align.horizontal,
-      -- Left
+  -- Draws the client titlebar at the default position (top) and size.
+  awful.titlebar(c, { size = 40 }).widget = wibox.widget({
+    layout = wibox.layout.align.horizontal,
+    -- Left
+    {
+      layout = wibox.layout.fixed.horizontal,
       {
-         layout  = wibox.layout.fixed.horizontal,
-         awful.titlebar.widget.iconwidget(c),
-         buttons = buttons
+        awful.titlebar.widget.iconwidget(c),
+        widget = wibox.container.margin,
+        margins = 8,
       },
-      -- Middle
+      buttons = buttons
+    },
+    nil,
+    -- Right
+    {
       {
-         layout  = wibox.layout.flex.horizontal,
-         { -- Title
-            widget = awful.titlebar.widget.titlewidget(c),
-            halign = 'center'
-         },
-         buttons = buttons
+        layout = wibox.layout.fixed.horizontal,
+        spacing = 10,
+        awful.titlebar.widget.maximizedbutton(c),
+        awful.titlebar.widget.closebutton(c)
       },
-      -- Right
-      {
-         layout = wibox.layout.fixed.horizontal,
-         awful.titlebar.widget.floatingbutton(c),
-         awful.titlebar.widget.maximizedbutton(c),
-         awful.titlebar.widget.stickybutton(c),
-         awful.titlebar.widget.ontopbutton(c),
-         awful.titlebar.widget.closebutton(c)
-      }
-   })
+      widget = wibox.container.margin,
+      margins = 8,
+    }
+  })
 end
